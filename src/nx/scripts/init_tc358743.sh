ad51e46bd19d472f5a15c9b1ba012712bfa74b7b
Eric Ahn <wchill1337@gmail.com>
2023-05-25 13:52:36 -0700

    Handle an extra ioctl error code in tc358743 init script

--- src/nx/scripts/init_tc358743.sh
+++ src/nx/scripts/init_tc358743.sh
@@ -3,7 +3,8 @@ DIR="$( dirname -- "${BASH_SOURCE[0]}"; )";   # Get the directory name
 DIR="$( realpath -e -- "$DIR"; )";    # Resolve its full path if need be
 /usr/bin/v4l2-ctl --device=/dev/hdmi-capture --set-edid=file=$DIR/edid.txt --fix-edid-checksums --info-edid
 
-while /usr/bin/v4l2-ctl --set-dv-bt-timings query 2>&1 | grep 'VIDIOC_S_DV_TIMINGS' &> /dev/null
+while /usr/bin/v4l2-ctl --set-dv-bt-timings query 2>&1 | grep 'VIDIOC_S_DV_TIMINGS\|VIDIOC_QUERY_DV_TIMINGS' &> /dev/null
 do
   echo "Sleeping 3 seconds"
+  sleep 3
 done
